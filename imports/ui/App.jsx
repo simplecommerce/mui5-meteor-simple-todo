01f4f9733f6e32ea007ae2dca95a5ec18c389464
root <kheang.hc@prosomo.com>
2021-10-18 05:01:37 -0400

    initial commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import FormDialog from "./FormDialog.jsx";
+import BasicMenu from "./BasicMenu.jsx";
+
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    <FormDialog />
+    <BasicMenu />
+  </div>
+);
