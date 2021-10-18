3a666596a6c7698dfea58ffbceafa10161b2a620
root <kheang.hc@prosomo.com>
2021-10-18 06:30:39 -0400

    update

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import FormDialog from "./FormDialog.jsx";
 import BasicMenu from "./BasicMenu.jsx";
+import BasicModal from "./BasicModal.jsx";
+import TransitionsModal from "./TransitionsModal.jsx";
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
     <FormDialog />
     <BasicMenu />
+    <BasicModal />
+    <TransitionsModal />
   </div>
 );
